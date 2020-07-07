c5340391f38eed48c7da63a0ee72038d4185de3d
Gerald Bivens <geraldbivens@gmail.com>
2020-07-07 10:06:04 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -34,11 +34,31 @@ class Song
     def self.genre_count
         #returns a hash in which the keys are the names of each genre
         #value is the number of songs that have that genre
+        genre_count = {}
+
+        @@genres.each do |genre|
+          if genre_count[genre]  
+            genre_count[genre] += 1
+          else
+            genre_count[genre] = 1
+          end
+        end
+        genre_count
     end
     
     def self.artist_count
         #returns a hash in which the keys are the names of each artist
         #value is the number of songs that have that artist
+        artist_count = {}
+
+        @@artists.each do |artist|
+          if artist_count[artist]  
+            artist_count[artist] += 1
+          else
+            artist_count[artist] = 1
+          end
+        end
+        artist_count
     end
 end
 
